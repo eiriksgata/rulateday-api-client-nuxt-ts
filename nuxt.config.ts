13a8ea4026a173d1807e203cf44accd241be3533
Eiriksgata <2353686862@qq.com>
2023-07-18 17:39:35 +0800

    nuxt config add vuetify and vueuse nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  build: {
+    transpile: ["vuetify"],
+  },
   devtools: { enabled: true },
-  typescript:{
-    shim: false
-  }
-})
+  typescript: {
+    shim: false,
+  },
+  css: ["mdi/css/materialdesignicons.min.css"],
+  modules: ["@vueuse/nuxt"],
+});
