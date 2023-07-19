477b222b4f1795ddd97533e696255baac3c87689
Eiriksgata <2353686862@qq.com>
2023-07-19 14:09:22 +0800

    update vuetify icon font config

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,13 +1,20 @@
-import { createVuetify } from 'vuetify'
-import 'vuetify/styles'
+import { createVuetify } from "vuetify";
+import "vuetify/styles";
 import * as components from "vuetify/components";
-
+import "@mdi/font/css/materialdesignicons.css";
+import "typeface-roboto/index.css";
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
-    components
-    
+    components,
+    icons: {
+      defaultSet: "mdi",
+      aliases,
+      sets: {
+        mdi,
+      }
+    }
   });
-
   nuxtApp.vueApp.use(vuetify);
 });
