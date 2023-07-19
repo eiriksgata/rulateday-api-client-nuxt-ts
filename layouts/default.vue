524b7c1c212a1030a7a06aa286af2280b802e43e
Eiriksgata <2353686862@qq.com>
2023-07-19 14:08:50 +0800

    update layer head bar

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-app-bar>
+
+      <v-btn prepend-icon="mdi-home" style="margin-left: 50px;" @click="$router.push('/')">
+        <template v-slot:prepend>
+          <v-icon color=""></v-icon>
+        </template>
+        工具主页
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn color="info">登陆</v-btn>
+    </v-app-bar>
+    <v-main style="background-color: #bbe6d6">
+
+      <slot />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+</script>
