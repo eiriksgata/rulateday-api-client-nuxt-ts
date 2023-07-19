0cfb48aa8c6b621c7f150e6911d35d7fdadc0f3a
Eiriksgata <2353686862@qq.com>
2023-07-19 14:09:03 +0800

    update '/' page

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,12 @@
 <template>
-  <div>
-    <v-app>
-      <!-- APP BAR -->
-      <v-app-bar app>
-
-        <v-btn prepend-icon="mdi-home" style="margin-left: 50px;">
-          <template v-slot:prepend>
-            <v-icon color=""></v-icon>
-          </template>
-          工具主页
-
-        </v-btn>
-
-        <v-spacer></v-spacer>
-        <v-btn color="info">登陆</v-btn>
-
-
-      </v-app-bar>
-
-      <v-main>
-        <v-container grid-list-xs>
-          <NavigationTool />
-        </v-container>
-      </v-main>
-
-    </v-app>
-  </div>
+  <v-container grid-list-xs>
+    <NavigationTool />
+  </v-container>
 </template>
 
 <style lang="sass" scoped>
 </style>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 
 </script>
